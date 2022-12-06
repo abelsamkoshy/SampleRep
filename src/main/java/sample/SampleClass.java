d8c4b6d02f331b9adf0dee35e6299cc958a85970
Abel Sam Koshy <abel.sam.koshy1947@gmail.com>
2022-12-06 19:21:30 +0530

    Initial Commit

---
+++ src/main/java/sample/SampleClass.java
@@ -0,0 +1,9 @@
+package sample;
+
+import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
+
+public class SampleClass {
+    public static void main(String[] args) {
+        System.out.println("Print");
+    }
+}
